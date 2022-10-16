d0c267f0a6357eb22e474cb4d3fa2bc3a953d613
LautaroCesso <lautaro_cesso@hotmail.com>
2022-10-16 01:27:25 -0300

    Add some info to about us panel

--- src/panels/about-us-panel.tsx
+++ src/panels/about-us-panel.tsx
@@ -1,12 +1,59 @@
+import Member, { MemberProps } from "../app-components/member";
 import Card from "../core-components/card";
+import Header from "../core-components/header";
 
 import "./about-us-panel.scss";
 
 function AboutUsPanel() {
+  const membersProps: MemberProps[] = [
+    {
+      fullname: "Lautaro Cesso",
+      headline: "React Developer",
+      profilePic: "lcesso.jpeg",
+      socialNetworks: [
+        { type: "linkedin", profile: "lautaro-cesso" },
+        { type: "github", profile: "lautarocesso" },
+        { type: "instagram", profile: "lautaro27c" },
+      ],
+    },
+    {
+      fullname: "Joel Mendez",
+      headline: "React Developer",
+      profilePic: "default.jpeg",
+      socialNetworks: [
+        { type: "linkedin", profile: "joeleliasmendez" },
+        { type: "github", profile: "joelmendez1" },
+      ],
+    },
+    {
+      fullname: "Leonel Redigonda",
+      headline: "React Developer",
+      profilePic: "default.jpeg",
+      socialNetworks: [
+        { type: "linkedin", profile: "leonel-redigonda-809116194/" },
+        { type: "github", profile: "Lredigonda" },
+      ],
+    },
+  ];
+
   return (
     <div className="about-us-panel">
       <Card className="about-us-panel__card" withBorder>
-        <div>About Us</div>
+        <>
+          <Header>About Us</Header>
+          <div className="about-us-panel__members">
+            {membersProps.map((membersProp, index) => {
+              const { fullname, profilePic } = membersProp;
+
+              return (
+                <Member
+                  key={`member-${fullname}-pic-${profilePic}__${index}`}
+                  {...membersProp}
+                />
+              );
+            })}
+          </div>
+        </>
       </Card>
     </div>
   );
